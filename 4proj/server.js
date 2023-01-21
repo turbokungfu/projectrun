d1330f075238bc4a034150eafe5d23011ccf74f6
turbokungfu <slashrandazzo@gmail.com>
2023-01-21 09:35:26 -0800

    first commit

---
+++ 4proj/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const engine = require('express-handlebars')
+const app = express();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
