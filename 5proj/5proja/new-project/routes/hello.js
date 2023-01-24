abb0fd6e927bc7b7ee7cffe14c44081261aa7c8a
turbokungfu <slashrandazzo@gmail.com>
2023-01-24 11:10:03 -0800

    finished routes

--- 5proj/5proja/new-project/routes/hello.js
+++ 5proj/5proja/new-project/routes/hello.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router();
+const crypto = require('crypto')
 
-router.get('/hello', (req, res, next) => {
-    const name = req.query.name;
-    res.render('hello', {name})
+router.get('/time', (req, res, next)=> {
+    res.json({
+        time: new Date().toISOString()
+    })
+})
+
+router.post('/hash', (req, res, next)=> {
+    const plainText = req.body.plainText;
+    const hash = crypto.createHash('md5').update(plainText).digest('hex');
+    res.json({
+        plainText,
+        hash,
+    })
+})
+
+router.get('/:person/:greeting', (req, res, next) => {
+    const name = req.params.person;
+    const greeting = req.params.greeting;
+    res.render('hello', {name, greeting})
 })
 
 module.exports = router;
