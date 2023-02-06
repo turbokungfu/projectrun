f8975a2f85aa0781d72c9cdc90f68a2ec99a0059
turbokungfu <slashrandazzo@gmail.com>
2023-02-05 16:28:25 -0800

    added deno and fresh

---
+++ 15proj/fresh/tryit/README.md
@@ -0,0 +1,14 @@
+# fresh project
+
+### Usage
+
+Start the project:
+
+```
+deno task start
+```
+
+This will watch the project directory and restart as necessary.
+
+
+# I downloaded and started the fresh app. It works, but the editor shows me weird react errors and I've tried all the fixes on the internet. I'm tired from being up awhile and may just look up stuff on the internet. I'll have to get used to the concept of islands.
---
+++ 15proj/practice.ts
@@ -0,0 +1,13 @@
+export function simpleMultiplication(num: number): number{
+    console.log(num)
+    return num % 2 === 0 ? num * 8 : num * 9;
+
+  }
+  //okay, I think the first number in the parameter is the variable name and the 2nd is the type, but I don't know what the 2nd 'number' means. I'll ask chatgpt.
+
+  //ok, the second number is the expected output of the function.
+
+  //I learned about 'unreachable' code that I tried to enter after the return.
+
+  //I'm going to try and make a deno and fresh framework now.
+
