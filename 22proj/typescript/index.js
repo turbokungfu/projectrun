4519ba26e7e0767dc60c834029de23584a98a084
turbokungfu <slashrandazzo@gmail.com>
2023-02-21 16:12:40 -0800

    some typescript

---
+++ 22proj/typescript/index.js
@@ -0,0 +1,6 @@
+console.log('i love tacos');
+var man = 'herb';
+console.log('i love tacos');
+console.log(man);
+
+console.log('string'.length)
---
+++ 22proj/typescript/index.ts
@@ -0,0 +1,7 @@
+console.log('i love tacos')
+const man = 'herb'
+console.log('i love tacos')
+console.log(man)
+console.log('string'.length)
+console.log('string'.length * 'food'.length)
+console.log(19.58.toPrecision())
