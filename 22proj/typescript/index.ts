62352ba7d563d6c419d53465bbdbd92f504a500c
turbokungfu <slashrandazzo@gmail.com>
2023-02-22 11:34:12 -0800

    codewar

--- 22proj/typescript/index.ts
+++ 22proj/typescript/index.ts
@@ -5,3 +5,7 @@ console.log(man)
 console.log('string'.length)
 console.log('string'.length * 'food'.length)
 console.log(19.58.toPrecision())
+
+function discoverOriginalPrice(discount, sale){
+    return (discount/(1-(sale*.01))).toFixed(2)*1
+  }
